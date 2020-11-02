16db27b0a0b88d89301f6fa6174e0c32adeb7df5
Timmy <adefeyitimi@gmail.com>
2020-11-02 12:44:37 +0100

    Created our controller and set up our view

---
+++ controllers/todo_controller.js
@@ -0,0 +1,28 @@
+// Import bodyparser module
+
+var bodyParser = require('body-parser');
+var urlencoderParser = bodyParser.urlencoded({extended:false});
+var data = [{
+    item:'Finish Project'
+},{
+    item:'Finish Writing the Novel'
+},{
+    item:'Edit Videos'
+}]
+
+module.exports = (app)=> {
+    // Set up our routes
+    app.get('/todo',(req,res)=>{
+        res.render('todo',{todos:data})
+    });
+    app.post('/todo',urlencoderParser,(req,res)=>{
+        data.push(req.body);
+        res.json(data)
+    });
+    app.delte('/todo/:item',(req,res)=>{
+        data = data.filter((todo)=>{
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
+    })
+}
